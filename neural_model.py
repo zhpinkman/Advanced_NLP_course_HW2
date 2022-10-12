1f6b86d353d282e9ed5188ced947ace883e1350b
Zhivar <zhivarsourati@gmail.com>
2022-10-12 03:52:50 +0200

    change the embedding vector reader

--- neural_model.py
+++ neural_model.py
@@ -22,7 +22,7 @@ def load_vectors(fname):
     fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
     data = {}
     for line in fin:
-        tokens = line.rstrip().split(' ')
+        tokens = line.rstrip().split()
         data[tokens[0]] = np.array(list(map(float, tokens[1:])))
     return data
 
